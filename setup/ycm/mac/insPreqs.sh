a5742051129cec749a76fa06c7661e57ecb0657c
cmdline-batcheloranator <84193135+cmdline-batcheloranator@users.noreply.github.com>
2021-06-27 03:59:01 +0100

    Update insPreqs.sh

--- setup/ycm/mac/insPreqs.sh
+++ setup/ycm/mac/insPreqs.sh
@@ -1,8 +1,11 @@
-brew install           \
-            go         \
-            mono       \
-            ctags      \
-            cmake      \
-            nodejs     \
-            python     \
-            vim --HEAD
+pkgs=(
+  'go'
+  'mono'
+  'ctags'
+  'cmake'
+  'nodejs'
+  'python'
+  'vim --HEAD'
+ )
+
+parallel -j 0 brew install ::: ${pkgs[@]}
